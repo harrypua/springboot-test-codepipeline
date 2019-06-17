c2229f2cda8bd79a3ca75c3e4d45621a51394e79
Harry Pua <harrypua@gmail.com>
2019-06-17 19:33:57 +0800

    added test route

---
+++ src/main/java/com/example/test/springbootapp/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.test.springbootapp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testRoute() {
+        return "test ok!";
+    }
+}
--- src/test/java/com/example/test/springbootapp/SpringbootAppApplicationTests.java
+++ src/test/java/com/example/test/springbootapp/SpringbootAppApplicationTests.java
@@ -2,15 +2,25 @@ package com.example.test.springbootapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.test.springbootapp.controller.TestController;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(TestController.class)
 public class SpringbootAppApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception{
+		mvc.perform(get("/test/")).andExpect(status().isOk());
 	}
 
 }
