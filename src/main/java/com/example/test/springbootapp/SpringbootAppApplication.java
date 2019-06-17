699b316dee718d96ebfefe954980a5aa63c2a5d7
Harry Pua <harrypua@gmail.com>
2019-06-17 15:56:59 +0800

    added springboot base

---
+++ src/main/java/com/example/test/springbootapp/SpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppApplication.class, args);
+	}
+
+}
